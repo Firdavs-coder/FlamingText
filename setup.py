3afa0c1997dca7f5317f24fc9596556791b5d530
Firdavs-coder <shodiyev-f@mail.ru>
2023-03-27 22:51:50 +0500

    0.0.5 version

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, find_packages
+import codecs
+import os
 
-VERSION = '0.0.3'
+VERSION = '0.0.5'
 DESCRIPTION = 'Unofficial API of flamingtext.com'
-LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+long_description = 'My first Python package with a slightly longer description'
+
+here = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name="FlamingText",
     version=VERSION,
     author="Firdavs Shodiyev",
     author_email="firdavscoder1@gmail.com",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Firdavs-coder/FlamingText",
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
     install_requires=["requests"],
     keywords=['python', 'effects', 'text', 'flamingtext'],
